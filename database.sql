e330c892e77183370ad96036d0b07e0a955d1850
andie03 <fenitratosuke@gmail.com>
2026-01-20 12:55:38 +0300

    Creation de la base de donnees

---
+++ database.sql
@@ -0,0 +1,53 @@
+-- Creation des statut en enumeration
+
+CREATE TYPE statut_cours AS ENUM ('non_commence', 'en_cours', 'termine');
+CREATE TYPE statut_seance AS ENUM ('annule', 'prevu', 'en_cours');
+
+--creation des tables dans la base de donnees
+
+--table classe
+
+CREATE TABLE classe (
+    id_classe INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nom_classe VARCHAR (100)
+);
+
+--table prof
+
+CREATE TABLE prof(
+    id_prof INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nom_prof VARCHAR (100),
+    nb_abs INTEGER DEFAULT 0
+);
+
+-- table matiere
+
+CREATE TABLE matiere(
+    id_matiere INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nom_matiere VARCHAR (100),
+    heure_totale INTEGER,
+    id_classe INTEGER,
+    CONSTRAINT fk_classe FOREIGN KEY (id_classe) REFERENCES classe (id_classe)
+);
+
+-- table cours (table ou admin lie prof et admin)
+CREATE TABLE cours(
+    id_cours INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    statut statut_cours DEFAULT 'non_commence',
+    id_matiere INTEGER,
+    id_prof INTEGER,
+    cumul INTEGER,
+    CONSTRAINT fk_matiere FOREIGN KEY (id_matiere) REFERENCES matiere(id_matiere),
+    CONSTRAINT fk_prof FOREIGN KEY (id_prof) REFERENCES prof(id_prof)
+);
+
+-- table sceance
+CREATE TABLE seance(
+    id_seance INTEGER ALWAYS GENERATED AS IDENTITY PRIMARY KEY,
+    id_cours INTEGER,
+    date_seance DATE,
+    heure_debut TIME,
+    heure_fin TIME,
+    statut statut_seance DEFAULT 'prevu',
+    CONSTRAINT fk_cours FOREIGN KEY (id_cours) REFERENCES cours(id_cours)
+);
