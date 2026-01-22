25d2d487b6a1d74bde354c3b954b53a4c751a3d6
andie03 <fenitratosuke@gmail.com>
2026-01-22 10:18:24 +0300

    Simulation de base de donnees matiere

---
+++ simulation_matiere.sql
@@ -0,0 +1,56 @@
+INSERT INTO matiere (nom_matiere, heure_totale, id_classe) VALUES
+-- =====================
+-- CLASSE DE 6e (id_classe = 1)
+-- =====================
+('Français', 160, 1),
+('Mathématiques', 150, 1),
+('Histoire-Géographie', 120, 1),
+('Sciences de la Vie et de la Terre', 90, 1),
+('Physique-Chimie', 60, 1),
+('Anglais', 120, 1),
+('Éducation Physique et Sportive', 100, 1),
+('Technologie', 60, 1),
+('Arts Plastiques', 50, 1),
+('Éducation Musicale', 50, 1),
+
+-- =====================
+-- CLASSE DE 5e (id_classe = 2)
+-- =====================
+('Français', 150, 2),
+('Mathématiques', 140, 2),
+('Histoire-Géographie', 120, 2),
+('Sciences de la Vie et de la Terre', 100, 2),
+('Physique-Chimie', 80, 2),
+('Anglais', 120, 2),
+('Éducation Physique et Sportive', 100, 2),
+('Technologie', 70, 2),
+('Arts Plastiques', 50, 2),
+('Éducation Musicale', 50, 2),
+
+-- =====================
+-- CLASSE DE 4e (id_classe = 3)
+-- =====================
+('Français', 140, 3),
+('Mathématiques', 140, 3),
+('Histoire-Géographie', 120, 3),
+('Sciences de la Vie et de la Terre', 90, 3),
+('Physique-Chimie', 90, 3),
+('Anglais', 120, 3),
+('Éducation Physique et Sportive', 100, 3),
+('Technologie', 70, 3),
+('Arts Plastiques', 50, 3),
+('Éducation Musicale', 50, 3),
+
+-- =====================
+-- CLASSE DE 3e (id_classe = 4)
+-- =====================
+('Français', 140, 4),
+('Mathématiques', 140, 4),
+('Histoire-Géographie', 130, 4),
+('Sciences de la Vie et de la Terre', 100, 4),
+('Physique-Chimie', 100, 4),
+('Anglais', 120, 4),
+('Éducation Physique et Sportive', 100, 4),
+('Technologie', 70, 4),
+('Arts Plastiques', 40, 4),
+('Éducation Musicale', 40, 4);
